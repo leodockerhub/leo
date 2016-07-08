a6236c5f97a99ba51c2aa554c726c3b0742cf0bf
leodockerhub <leo.souillard@gmail.com>
2016-07-08 09:37:34 +0200

    Update deploiement.sh

--- deploiement.sh
+++ deploiement.sh
@@ -1,6 +1,10 @@
 sudo apt-get install mysql-client
-docker pull leodockerhub/mysql
+sudo docker pull leodockerhub/mysql
 sudo wget -P /home/ https://raw.githubusercontent.com/leodockerhub/leo/master/docker-compose.yml
 sudo wget -P /home/ https://raw.githubusercontent.com/leodockerhub/leo/master/todoVal.sql
 cd /home/
 sudo docker-compose up -d
+sleep 10s
+ip=$(wget -q -O - http://ip.tupeux.com | tail)
+sudo mysql -h $ip -u root --password=azerty -e "CREATE DATABASE todoVal"
+sudo mysql -h $ip -u root --password=azerty todoVal < todoVal.sql
